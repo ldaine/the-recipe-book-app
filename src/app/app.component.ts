642b6cd5cf6714ac04698b3c7e44e14088a43a8f
Liga Daine <liga.daine@daine-it.com>
2018-08-25 20:33:53 +0200

    firebase added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Platform, MenuController, NavController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import firebase from 'firebase'; 
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -15,12 +17,19 @@ export class MyApp {
   @ViewChild('nav') nav:NavController;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private menuCtrl: MenuController) {
+    firebase.initializeApp({
+      apiKey: "AIzaSyA5QQabKoZUqOtjd-RorUhxtFpcyuQXrD8",
+      authDomain: "the-recipe-book-59041.firebaseapp.com"
+    }); 
+    
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+     
   }
 
   onLoad(page:any){
