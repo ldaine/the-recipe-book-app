1bd81e663b57390551d2e2e814c3b04881a01bb1
Liga Daine <liga.daine@daine-it.com>
2018-08-25 20:54:31 +0200

    signin feature

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,6 +1,7 @@
+import { AuthService } from './../../services/auth';
 import { TabsPage } from './../tabs/tabs';
 import { NgForm } from '@angular/forms';
-import { IonicPage, NavController } from 'ionic-angular';
+import { IonicPage, NavController, LoadingController, AlertController } from 'ionic-angular';
 import { Component, OnInit } from '@angular/core';
 
 @IonicPage()
@@ -11,14 +12,36 @@ import { Component, OnInit } from '@angular/core';
 export class SigninPage implements OnInit{
 
 
-    constructor(private navCtrl: NavController){}
+    constructor(private navCtrl: NavController, 
+                private authService: AuthService, 
+                private loadingCtrl: LoadingController, 
+                private alertCtrl: AlertController
+            ){}
 
     ngOnInit(){
         
     }
 
     onLogin(form: NgForm){
-        this.navCtrl.push(TabsPage); 
-        //console.log('on login'); 
+        const loading = this.loadingCtrl.create({
+            content: "Signing you in ..."
+        }); 
+    
+        loading.present(); 
+        this.authService.signin(form.value.email, form.value.password).then(data=>{
+            console.log(data); 
+            loading.dismiss(); 
+            //this.navCtrl.setRoot("LoginPage", form.value);
+        }).catch(error=>{
+            console.log(error); 
+            loading.dismiss(); 
+            const alert = this.alertCtrl.create({
+                title: "Signin failed", 
+                message: error.message, 
+                buttons: ['OK']
+            }); 
+            alert.present(); 
+        }); 
+           
     }
 }
